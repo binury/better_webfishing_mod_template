de881cc603b246f2dd5296b9cea0d510a3dd67f1
binury <1146921+binury@users.noreply.github.com>
2025-07-30 05:12:16 -0500

    Add: publishing script

---
+++ publish.ps1
@@ -0,0 +1,41 @@
+# Big thanks to Teemaw for putting this together !
+
+param(
+    [string]$Configuration = "Release",
+    [string]$ProjectPath = ".\GDWeave.Sample\MyModName.csproj",
+    [string]$GDWeavePath = "C:\Program Files (x86)\Steam\teamapps\common\WEBFISHING\GDWeave"
+)
+
+# Clean and build
+$env:GDWeavePath = $GDWeavePath
+dotnet clean $ProjectPath --configuration $Configuration
+dotnet restore $ProjectPath
+dotnet build $ProjectPath --configuration $Configuration --no-restore
+
+# Update Thunderstore manifest
+$ThunderstoreManifestPath = ".\thunderstore\manifest.json"
+$GDWeaveManifestPath = ".\GDWeave.Sample\manifest.json"
+$version = (Get-Content $GDWeaveManifestPath | ConvertFrom-Json).Metadata.Version
+$manifest = Get-Content $ThunderstoreManifestPath | ConvertFrom-Json
+$manifest.version_number = $version
+$manifest | ConvertTo-Json -Depth 1 | Set-Content $ThunderstoreManifestPath
+
+# Zip it up
+$gitTagOrHash = if (git describe --exact-match --tags HEAD 2>$null) {
+    git describe --exact-match --tags HEAD
+} else {
+    git rev-parse --short HEAD
+}
+$zipPath = ".\thunderstore\calico_$gitTagOrHash.zip"
+Compress-Archive -Path @(
+   ".\thunderstore\GDWeave",
+   ".\thunderstore\icon.png",
+   ".\thunderstore\manifest.json",
+   ".\thunderstore\CHANGELOG.md",
+   ".\thunderstore\README.md"
+) -DestinationPath $zipPath -Force
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Build failed LASTEXITCODE=$LASTEXITCODE"
+    exit $LASTEXITCODE
+}
---
+++ thunderstore/README.md
@@ -0,0 +1,25 @@
+# MyModName
+
+A brief description of your mod. Images can be useful here especially if your mod has some cosmetic components. This is your
+chance to stop their doomscroll and show them why they should
+start using your mod rite meow.
+
+<img src="https://i.imgur.com/mim5iuA.png" width="400">
+
+(But like, a picture of your mod. Pls no shitpost memes)
+
+## Usage
+How to use your mod. Sometimes users read this... I think. They
+definitely don't like reading though so be clear and concise and 
+have a little fun if you want.
+
+## Known Issues
+- Isn't done yet
+- 
+
+## Roadmap
+- Faster, stronger, better
+- Paid DLC
+-
+
+## [Changelog](https://thunderstore.io/c/webfishing/p/YOUR_ORG_NAME/YOUR_MOD_NAME/changelog)
